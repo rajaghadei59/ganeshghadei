216a599cfe63fbae86edb4c8c9545b692d045689
ganesh <ganesh@192.168.2.13>
2019-10-25 20:56:45 +0530

    second commit change in employee classs

--- src/main/java/ganeshghadei/Employee.java
+++ src/main/java/ganeshghadei/Employee.java
@@ -3,5 +3,18 @@ package ganeshghadei;
 public class Employee {
 	private Integer id;
 	private String empName;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	
 
 }
